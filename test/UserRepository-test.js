b85191c5c98a0e4fe8f57acf778b573fce658e12
Your Name <stoner.michann@gmail.com>
2021-03-27 17:08:32 -0600

    Create first two tests in UserRepository

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -2,3 +2,16 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const UserRepository = require('../src/UserRepository');
+
+describe('UserRepository', () => {
+
+  it('should be a function', () => {
+    expect(UserRepository).to.be.a('function');
+  });
+
+  it('should be an instance of UserRepository', () => {
+    const userRepo = new UserRepository();
+
+    expect(userRepo).to.be.an.instanceOf(UserRepository);
+  });
+})
