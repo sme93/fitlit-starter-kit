1d5a548ae5f590005dea76c527ebade2301f3799
Sarah Fitzsimons <sarahfitz2713@gmail.com>
2021-03-28 15:29:45 -0600

    Add querySelector and eventListeners for Greeting and user info

--- src/User.js
+++ src/User.js
@@ -14,4 +14,4 @@ class User {
   }
 }
 
-module.exports = User;
+//module.exports = User;
--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+//const User = require('../src/User');
+// import User from "./User"
+// eslint-disable-next-line no-undef
+const user = new User(userData[0]);
+
+const userNameGreeting = document.querySelector('#userGreeting');
+const userInformationSection = document.querySelector('#userInformationSection');
+
+
+
+const greetUser = () => {
+  console.log(user);
+  userNameGreeting.innerHTML = `Hi, ${user.getFirstName()}!`
+}
+
+const displayUserInformation = () => {
+    const markup = `
+    <section class="user-info-card">
+      <p><span>Full Name: </span><span>${user.name}</span></p>
+      <p><span>Address: </span><span>${user.address}</span></p>
+      <p><span>Email: </span><span>${user.email}</span></p>
+      <p><span>Stride Length: </span><span>${user.strideLength}</span></p>
+      <p><span>Daily Step Goal: </span><span>${user.dailyStepGoal}</span></p>
+    </section>`
+  userInformationSection.innerHTML = markup;
+}
+
+window.addEventListener('load', greetUser);
+window.addEventListener('load', displayUserInformation);
