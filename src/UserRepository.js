78e6336a2ffaf7bc60d56f67aa0cac1d9340a647
Your Name <stoner.michann@gmail.com>
2021-03-28 12:19:26 -0600

    Create tests for methods in UserRepository and some trial implementation code

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,7 +1,19 @@
 class UserRepository {
-  constructor(user) {
-    this.user = user;
+  constructor(data) {
+    this.userData = data;
+  };
+  findUserData = id => {
+    // if (this.userData.id === id) {
+    //   return this.data;
+    // }
+    // const foundUser = this.userData.find(id => {
+    //   return this.userData;
+    // });
+    // console.log(foundUser);
+    // return foundUser;
+  };
+  calculateAvgStepGoal = () => {
   }
-}
+};
 
 module.exports = UserRepository;
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,16 +1,20 @@
 const chai = require('chai');
-const User = require('../src/User');
 const expect = chai.expect;
 
 const UserRepository = require('../src/UserRepository');
+const User = require('../src/User');
 
 describe('UserRepository', () => {
   let userRepo;
-  let user;
+  let user1;
+  let user2;
+  let user3;
 
   beforeEach(() => {
-    userRepo = new UserRepository();
-    user = new User(1, 'Michann Stoner', '1234 House Ln, Denver CO 80211-1234, michann.stoner@gmail.com', 4.1, 100, [2, 10, 20])
+    user1 = new User(1, 'Michann Stoner', '1234 House Ln, Denver CO 80211-1234', 'michann.stoner@gmail.com', 4.1, 100, [2, 10, 20]);
+    user2 = new User(2, 'Sarah Fitzsimons', '4321 House St, Loveland CO 80246-1234', 'sarah.f@gmail.com', 4.3, 2000, [3, 15, 1]);
+    user3 = new User(3, 'John Doe', '5555 Apartment Rd, Denver CO 80211-4321', 'johndoe@outlook.com', 3.2, 1000, [4, 20, 17]);
+    userRepo = new UserRepository([user1, user2, user3]);
   });
 
   it('should be a function', () => {
@@ -18,13 +22,21 @@ describe('UserRepository', () => {
   });
 
   it('should be an instance of UserRepository', () => {
-
     expect(userRepo).to.be.an.instanceOf(UserRepository);
   });
 
   it('should hold user data', () => {
-    const userRepo1 = new UserRepository(user);
+    expect(userRepo.userData).to.deep.equal([user1, user2, user3]);
+  });
 
-    expect(userRepo1).to.equal(user);
+  it.skip('should return user data when given a user ID', () => {
+    const userData1 = userRepo.findUserData(1);
+    
+    expect(userData1).to.deep.equal(user1);
+  });
+
+  it('should calculate average step goal amongst all users', () => {
+    const avgGoal = userRepo.calculateAvgStepGoal();
+    expect(avgGoal).to.equal(1033);
   });
 })
