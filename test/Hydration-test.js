93cd861c20486c52ef76d564f5f27922af2343f6
Your Name <stoner.michann@gmail.com>
2021-03-30 20:18:19 -0600

    Finish tests for Hydration

--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -58,4 +58,55 @@ describe('Hydration', () => {
     const ouncesConsumed = hydration.returnOuncesByDate('2019/06/15');
     expect(ouncesConsumed).to.equal(20);
   });
+
+  it('should return ounces consumed per day, over a week', () => {
+    const hydrationData = [
+      {
+        "userID": 1,
+        "date": "2019/06/15",
+        "numOunces": 20
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/16",
+        "numOunces": 25
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/17",
+        "numOunces": 30
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/18",
+        "numOunces": 40
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/19",
+        "numOunces": 45
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/20",
+        "numOunces": 50
+      },
+      {
+        "userID": 1,
+        "date": "2019/06/21",
+        "numOunces": 55
+      }
+    ];
+    const hydration1 = new Hydration(1, hydrationData);
+      const weeklyOunces = hydration1.findWeeklyFluidIntake('2019/06/15');
+      expect(weeklyOunces).to.equal({
+        '2019/06/15': 20,
+        '2019/06/16': 25,
+        '2019/06/17': 30,
+        '2019/06/18': 40,
+        '2019/06/19': 45,
+        '2019/06/20': 50,
+        '2019/06/21': 55
+      });
+  });
 })
